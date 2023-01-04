b60c23f3997680b5c49fc8827bda99d460630d4a
haudtr285 <thehauhuynh5@gmail.com>
2023-01-04 13:05:56 +0700

    use express

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const http = require('http')
 
-const server = http.createServer((req, res) => {
-    console.log("run request...")
-    res.setHeader('Content-Type', 'text/html')
-    res.write("<h1>Hello world!</h1>")
-    res.end()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
 })
 
-server.listen(3000, "localhost", () => {
-    console.log('Node.JS sever is running on port: 3000')
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
 })
