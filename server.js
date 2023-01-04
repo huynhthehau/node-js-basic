01347e613516cdb5250b8850658c361d066d4e38
haudtr285 <thehauhuynh5@gmail.com>
2023-01-04 12:33:26 +0700

    use npm  install express

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const http = require('http')
 
-const sever = http.createServer((req, res) => {
+const server = http.createServer((req, res) => {
     console.log("run request...")
     res.setHeader('Content-Type', 'text/html')
     res.write("<h1>Hello world!</h1>")
     res.end()
 })
 
-sever.listen(3000, "localhost", () => {
+server.listen(3000, "localhost", () => {
     console.log('Node.JS sever is running on port: 3000')
 })
