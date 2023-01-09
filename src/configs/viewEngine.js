1ddc5261de5e7efaf4a3846da4f1d50f9793bfc4
haudtr285 <thehauhuynh5@gmail.com>
2023-01-09 14:54:22 +0700

    set view engine ejs

---
+++ src/configs/viewEngine.js
@@ -0,0 +1,10 @@
+
+
+const configViewEngine = app => {
+    app.set("view engine", "ejs")
+    // Set view engine
+    app.set("views", "./src/views")
+    // find file ejs
+}
+
+export default configViewEngine
